93c94f04999d43dcb99718fcf0251e3dfe3a109e
ogulcancicek <ogulcanicek@gmail.com>
2025-09-30 09:41:56 +0300

    fixed absolute path issue and add new class

--- features/preprocessing.py
+++ features/preprocessing.py
@@ -1,15 +1,20 @@
+import os
 import pandas as pd
 import numpy as nd
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
+from typing import List, Optional
+
 #df = pd.read_csv(r"C:\Users\ybayraktar\Desktop\Yeni klasör\sample_submission.csv")
-test_df = pd.read_csv(r"C:\\Users\\rbesli\\Desktop\\src\\data\\test.csv")
-train_df = pd.read_csv(r"C:\\Users\\rbesli\\Desktop\\src\\data\\train.csv")
+data_dir = os.path.abspath(os.path.join(os.getcwd(), "..", "data"))
+
+train_df = pd.read_csv(os.path.join(data_dir, "train.csv"))
+test_df = pd.read_csv(os.path.join(data_dir, "test.csv"))
 
 
 
@@ -34,3 +39,62 @@ def standardScaler(df : pd.DataFrame):
 
     X_train_scaled[numeric_cols] = scaler.fit_transform(X_train[numeric_cols])
     X_valid_scaled[numeric_cols] = scaler.transform(X_valid[numeric_cols])
+
+class Preprocessor:
+    def __init__(self, n_components: Optional[int] = None):
+        self.pca = PCA(n_components=n_components)
+        self.scaler = StandardScaler()
+        self.encoding_dict = dict()
+
+    def scale_features(
+        self, X: pd.DataFrame, features_to_scale: List[str]
+    ) -> pd.DataFrame:
+        result = X.copy()
+        result[features_to_scale] = self.scaler.fit_transform(X[features_to_scale])
+        return result
+
+    def pca_fit(
+        self, X: pd.DataFrame, features: List, n_components=None
+    ) -> pd.DataFrame:
+        result = X[features].copy()
+
+        for col in features:
+            if result[col].dtypes == "object":
+                print(
+                    f"Detected a categorical column ({col}), applying LabelEncoding..."
+                )
+                self.encoding_dict[col] = LabelEncoder()
+                result[col] = self.encoding_dict[col].fit_transform(result[col])
+
+        result = pd.DataFrame(
+            self.scale_features(result, result.columns),
+            columns=result.columns,
+            index=result.index,
+        )
+        self.pca.fit(X=result)
+
+    def pca_transform(
+        self, X: pd.DataFrame, first_n_components: Optional[int] = None
+    ) -> pd.DataFrame:
+        if len(self.pca.feature_names_in_) == 0:
+            raise RuntimeError("Call pca_fit before pca_transform.")
+
+        X_num = X[self.pca.feature_names_in_.tolist()].copy()
+        for col in X_num.columns:
+            if X_num[col].dtype == "object":
+                if col in self.encoding_dict:
+                    X_num[col] = self.encoding_dict[col].transform(X_num[col])
+                else:
+                    raise ValueError("Detected categorical columns...")
+
+        Xs = self.scaler.transform(X_num)
+        Z = self.pca.transform(Xs)
+
+        k = (
+            Z.shape[1]
+            if first_n_components is None
+            else min(first_n_components, Z.shape[1])
+        )
+        Z = Z[:, :k]
+        cols = [f"PC{i+1}" for i in range(k)]
+        return pd.DataFrame(Z, index=X.index, columns=cols)
